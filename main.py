3985930e3b676e8f6442620b3cd3532991e740e2
victormoraes <victor.moraesrj@gmail.com>
2024-04-24 15:59:28 -0300

    Using Matplot Bar Graphs

--- main.py
+++ main.py
@@ -2,25 +2,26 @@ import numpy as np
 from die import Die
 from die_rolls import die_rolls
 
-import pygal
+import matplotlib.pyplot as plt
 
 
 def main():
-    die_list = [Die(), Die()]
-    rolls = 15000
-    operation = '+'
+    die_list = [Die(12), Die()]
+    rolls = 100000
+    operation = '*'
 
     frequency, max_result = die_rolls(rolls=rolls, dies=die_list, operation=operation)
+    die_numbers = list(range(len(die_list), max_result + 1))
+    label = operation.join([f"D{str(die.num_sides)}" for die in die_list])
 
-    hist = pygal.Bar()
-    hist.title = f"Results of rolling a D12 and a D12 {rolls} times."
 
-    hist.x_labels = list(range(len(die_list), max_result + 1))
-    hist.x_title = "Result"
-    hist.y_title = "Frequency of Result"
+    fig, ax = plt.subplots()
 
-    hist.add(operation.join(["D" + str(die.num_sides) for die in die_list]), frequency)
-    hist.render_to_file('dice_visual.svg')
+    ax.bar(die_numbers, frequency, label=label, color='red')
+    ax.set_ylabel('fruit supply')
+    ax.set_title(f'{label} {"{:,}".format(rolls)} times')
+    ax.legend(title='Dices')
+    plt.show()
 
 if __name__ == '__main__':
     main()
